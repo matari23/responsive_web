0c1fb4deffd9543568a00660bda7eec0e3e1932d
matari23 <matari23@naver.com>
2023-06-07 13:59:37 +0900

    반응형flex홈피완성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(function(){
-//토글버튼
-// $(".menu_toggle_btn").click(function(){
-//     $(".gnb").stop().slideDown();
-//     $(".gnb").stop().slideUp();
-// })
+//메뉴
+$(".menu_toggle_btn").click(function(){
+    $(".gnb").slideToggle()
+})//click
+
+//gallery cont_02 search_select_box span hover
+$(".search_select_box span, .search_select_list").hover(function(){
+    $(".search_select_list").stop().show();
+},function(){
+    $(".search_select_list").stop().hide();
+})
+
 })//jq
